66f6f200e00253d0e2871596c290519103d28e2b
Deep <sekharsmemories@gmail.com>
2021-06-08 01:46:15 +0530

    beginner project

--- JS/app.js
+++ JS/app.js
@@ -3,8 +3,10 @@ shownotes();
 
 let addbtn = document.getElementById("addbtn");
 addbtn.addEventListener("click", (e)=>{
+    // notes part 
     let addtxt= document.getElementById("addtxt");
     let notes = localStorage.getItem("notes");
+    let notesObj;
     if (notes==null) {
         notesObj = [];
     }
@@ -15,7 +17,23 @@ addbtn.addEventListener("click", (e)=>{
     notesObj.push(addtxt.value);
     localStorage.setItem("notes",JSON.stringify(notesObj));
     addtxt.value="";
-    console.log(notesObj);
+    shownotes();
+
+    // --------------------------------------
+    // header part 
+    let addhead= document.getElementById("addhead");
+    let heads = localStorage.getItem("heads");
+    let headObj;
+    if (heads==null) {
+        headObj = [];
+    }
+    else
+    {
+        headObj=JSON.parse(heads);
+    }
+    headObj.push(addhead.value);
+    localStorage.setItem("heads",JSON.stringify(headObj));
+    addhead.value="";
     shownotes();
 });
 
@@ -30,10 +48,33 @@ function shownotes()
     {
         notesObj=JSON.parse(notes);
     }
+
+    // ---------------------------------
+    // header part 
+    let addhead= document.getElementById("addhead");
+    let heads = localStorage.getItem("heads");
+    let headObj;
+    if (heads==null) {
+        headObj = [];
+    }
+    else
+    {
+        headObj=JSON.parse(heads);
+    }
+    // ---------------------------------
+    
     let html="";
-    notesObj.forEach((element, index) => {
-        html+=`<div class="notecard text-dark bg-warning mx-2 my-2"   style="max-width: 18rem;">
-        <div class="card-header"><h5>${index+1}</h5></div>
+    Array.from(notesObj).forEach((element, index) => {
+        let heading = headObj[index];
+
+        let color;
+        if (index%4==0) {color = "warning";}
+        if (index%4==1) {color = "success";}
+        if (index%4==2) {color = "primary";}
+        if (index%4==3) {color = "danger";}
+
+        html+=`<div class="notecard text-dark bg-${color} mx-2 my-2"   style="max-width: 18rem;">
+        <div class="card-header"><h5>${index+1}.${heading}</h5></div>
         <div class="card-body">
         <p class="card-text">${element}</p>
         <button id="${index}" href="#" onclick="deletenote(this.id)" class="btn btn-primary">Delete Note </button>
@@ -53,7 +94,7 @@ function shownotes()
 // delete notes 
 function deletenote(index)
 {
-    console.log(index);
+    // console.log(index);
     let notes = localStorage.getItem("notes");
     if (notes==null) {
         notesObj = [];
@@ -62,8 +103,25 @@ function deletenote(index)
     {
         notesObj=JSON.parse(notes);
     }
+
+    // ---------------------------------
+    // header part 
+    let addhead= document.getElementById("addhead");
+    let heads = localStorage.getItem("heads");
+    let headObj;
+    if (heads==null) {
+        headObj = [];
+    }
+    else
+    {
+        headObj=JSON.parse(heads);
+    }
+    // ---------------------------------
+
     notesObj.splice(index,1);
+    headObj.splice(index,1);
     localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("heads",JSON.stringify(headObj));
     shownotes();
 }
 
